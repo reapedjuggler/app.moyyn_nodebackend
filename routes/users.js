0bdbe0abf194a5e24646d1e7e31556971b2862b0
Pranav Agarwal <p2012agarwal@gmail.com>
2021-05-01 23:55:06 +0530

    updated

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer({dest:'./cvData'});
+var upload = multer({ dest: './cvData' });
 var expressValidator = require('express-validator');
 var bcrypt = require('bcryptjs');
 var fs = require('fs');
-const base64 = require('base64topdf');
+const base64 = require('js-base64');
 router.use(expressValidator());
 
 var User = require('../models/userModel');
@@ -13,111 +13,111 @@ var User = require('../models/userModel');
 var userData;
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('Candidates Home page');
 });
 
-router.get('/register', function(req, res, next) {
+router.get('/register', function (req, res, next) {
   res.send("This is GET register page, (send a POST request for registering user.)");
 });
 
-router.get('/login', function(req, res, next) {
+router.get('/login', function (req, res, next) {
   res.send("This is the GET login page");
 });
 
-router.post('/login', function(req, res){
+router.post('/login', function (req, res) {
 
   var email = req.body.email;
   var password = req.body.password;
 
-  User.getUserByEmail(email, function(err, user){
-    if(err) throw err;
-    if(!user){
-        res.send({
-        success:false,
-        msg:"Login Unsuccessful."
+  User.getUserByEmail(email, function (err, user) {
+    if (err) throw err;
+    if (!user) {
+      res.send({
+        success: false,
+        msg: "Login Unsuccessful."
       });
-    }else{
-      User.comparePassword(password, user.password, function(err, isMatch){
-        if(err) return done(err);
-        if(isMatch){
+    } else {
+      User.comparePassword(password, user.password, function (err, isMatch) {
+        if (err) return done(err);
+        if (isMatch) {
           console.log('You are now logged in Succesfully!');
           userData = user;
           res.send({
-            success:true,
-            msg:"Logged in",
-            candidate_id:user._id,
-            email:user.email,
-            name:user.firstName+" "+user.lastName
-         })
+            success: true,
+            msg: "Logged in",
+            candidate_id: user._id,
+            email: user.email,
+            name: user.firstName + " " + user.lastName
+          })
         } else {
           res.send({
-            success:false,
-            msg:"Error in Logging in", 
-        });
+            success: false,
+            msg: "Error in Logging in",
+          });
         }
       });
     }
   });
 });
 
-router.post('/resetpassword', function(req, res){
+router.post('/resetpassword', function (req, res) {
 
   var candidate_id = req.body.candidate_id;
   var newpassword = req.body.newpassword;
 
-  bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(newpassword, salt, function(err, hash) {
+  bcrypt.genSalt(10, function (err, salt) {
+    bcrypt.hash(newpassword, salt, function (err, hash) {
       // console.log(hash);
-       User.updateOne({_id:candidate_id}, {password: hash}, function(err, user){
-        if(err) throw err;
-        if(user.nModified == 0){
+      User.updateOne({ _id: candidate_id }, { password: hash }, function (err, user) {
+        if (err) throw err;
+        if (user.nModified == 0) {
           res.send({
-            success:false,
-            msg:"Unknown User"
-         });
-        }else{
+            success: false,
+            msg: "Unknown User"
+          });
+        } else {
           res.send({
-            success:true,
-            msg:"Updated Password"
-         });
+            success: true,
+            msg: "Updated Password"
+          });
         }
       });
     });
-});
+  });
 
 
 });
 
-router.post('/delete', async function(req, res){
+router.post('/delete', async function (req, res) {
 
   var candidate_id = req.body.candidate_id;
 
   await User.findByIdAndDelete(candidate_id, function (err, docs) {
-    if (err){
+    if (err) {
       res.send({
-        success:false,
-        error:err
+        success: false,
+        error: err
       })
     }
-    else{
-        res.send({
-          success:true,
-          msg:"Deleted Successfully."
-        })
+    else {
+      res.send({
+        success: true,
+        msg: "Deleted Successfully."
+      })
     }
-});
+  });
 
 
 });
 
 var cpUpload = upload.fields([{ name: 'cvEnglish', maxCount: 1 }, { name: 'cvGerman', maxCount: 1 }]);
 
-router.post('/getuser', async function(req, res){
+router.post('/getuser', async function (req, res) {
 
   var candidate_id = req.body.candidate_id;
 
-  User.findOne({_id:candidate_id}, function(err, user){
+  User.findOne({ _id: candidate_id }, function (err, user) {
     if (err) throw err;
 
     res.send(user);
@@ -126,23 +126,23 @@ router.post('/getuser', async function(req, res){
 
 
 
-router.post('/editprofile',cpUpload ,async function(req, res){
+router.post('/editprofile', cpUpload, async function (req, res) {
 
   var candidate_id = req.body.candidate_id;
 
-  var cvEnglish =req.body.cvEnglish;
-  var cvGerman =req.body.cvGerman || "File Not uploaded";
+  var cvEnglish = req.body.cvEnglish;
+  var cvGerman = req.body.cvGerman || "File Not uploaded";
 
   //to check if file is loaded correctly
-  if(req.file){
-    cvEnglish =req.file.cvEnglish;
-    cvGerman =req.file.cvGerman || "File Not uploaded";
-  }else{
+  if (req.file) {
+    cvEnglish = req.file.cvEnglish;
+    cvGerman = req.file.cvGerman || "File Not uploaded";
+  } else {
     // console.log("Error in uploading files.");
-    }
+  }
 
-    // console.log(req.files['cvEnglish'][0].filename);
-    // console.log(req.files['cvGerman'][0].filename);
+  // console.log(req.files['cvEnglish'][0].filename);
+  // console.log(req.files['cvGerman'][0].filename);
 
   var desiredPositions = req.body.desiredPositions;
   var relocationWillingnessFlag = req.body.relocationWillingnessFlag;
@@ -168,12 +168,12 @@ router.post('/editprofile',cpUpload ,async function(req, res){
   // Check Errors
   var errors = req.validationErrors();
 
-  if(errors){
-  	res.send({
-      success:false,
-      errors:errors,
+  if (errors) {
+    res.send({
+      success: false,
+      errors: errors,
     })
-  } else{
+  } else {
     var userData = {};
 
     userData.cvEnglish = req.files['cvEnglish'][0].filename;
@@ -199,40 +199,40 @@ router.post('/editprofile',cpUpload ,async function(req, res){
     // var newUser = new User(userData);
 
 
-  await User.updateOne({_id:candidate_id},{$set:userData}, function(err, user){
-    if (err){
-      res.send({
-        success:false,
-        msg:err
-      })
-    }else{
-      res.send({
-        success:true,
-        msg:"Updated Successfully"
-      })
-    }
+    await User.updateOne({ _id: candidate_id }, { $set: userData }, function (err, user) {
+      if (err) {
+        res.send({
+          success: false,
+          msg: err
+        })
+      } else {
+        res.send({
+          success: true,
+          msg: "Updated Successfully"
+        })
+      }
 
 
-  });
+    });
   }
 
 });
 
-router.post('/application',function(req, res, next) {
+router.post('/application', function (req, res, next) {
   var email = req.body.email;
 
-  User.getUserByEmail(email, function(err, user){
-    if(err) throw err;
+  User.getUserByEmail(email, function (err, user) {
+    if (err) throw err;
     console.log(user);
-    if(user == null){
+    if (user == null) {
       res.send({
-        success:true,
-        msg:"Account not found"
+        success: true,
+        msg: "Account not found"
       });
-    }else{
+    } else {
       res.send({
-        success:false,
-        msg:"Account found"
+        success: false,
+        msg: "Account found"
       });
     }
   });
@@ -241,103 +241,94 @@ router.post('/application',function(req, res, next) {
   // Check Errors
   var errors = req.validationErrors();
 
-  if(errors){
+  if (errors) {
     res.send({
-      success:false,
-      error:errors
+      success: false,
+      error: errors
     });
-  } else{
+  } else {
   }
 
 });
 
-router.post('/register' ,function(req, res, next) {
+router.post('/register', function (req, res, next) {
+  try {
+    var data = req.body;
+    // console.log(data[0].firstname);
+    var firstName = data[0].firstname;
+    var lastName = data[0].lastname;
+    var email = data[0].email;
+    var activeJobSeeking = data[0].activeJobSeeking || false;
+    var termsAndPrivacyFlag = data[0].termsAndPrivacyFlag;
+    var password = data[0].password;
 
-  try{
-  var data = req.body.data;
-  // console.log(data[0].firstname);
-  var firstName = data[0].firstname;
-  var lastName = data[0].lastname;
-  var email = data[0].email;
-  var activeJobSeeking = data[0].activeJobSeeking || false;
-  var termsAndPrivacyFlag = data[0].termsAndPrivacyFlag;
-  var password = data[0].password;
+    var cvEnglish = data[1].cvEnglish;
+    var cvGerman = data[1].cvGerman;
 
-  var cvEnglish =data[1].cvEnglish[0];
-  var cvGerman =data[1].cvGerman[0] || "File Not uploaded";
+    let decodedBase64English = "";
+    let decodedBase64German = "";
 
-  let decodedBase64German = base64.base64Decode(cvEnglish.data, "../cvData/German_CV/"+cvGerman.file);
-  let decodedBase64English = base64.base64Decode(cvGerman.data, "../cvData/English_CV/"+cvEnglish.file);
+    if (cvEnglish.data !== null) decodedBase64English = base64.atob(cvEnglish.data);
+    if (cvGerman.data !== null) decodedBase64German = base64.atob(cvGerman.data);
 
-  console.log(cvGerman.data);
-
-  fs.writeFile("../cvData/German_CV/"+cvGerman.file, decodedBase64German, function(err) {
-    if(err) {
-        return console.log(err);
+    if (cvEnglish.data !== null) {
+      fs.writeFile("cvData/English_CV/" + cvEnglish.fileName, decodedBase64English, 'binary', function (err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("English pdf saved!");
+      });
     }
-    console.log("German pdf saved!");
-});
 
-  fs.writeFile("../cvData/English_CV/"+cvEnglish.file, decodedBase64English, function(err) {
-  if(err) {
-      return console.log(err);
-  }
-  console.log("English pdf saved!");
-  });
+    if (cvGerman.data !== null) {
+      fs.writeFile("cvData/German_CV/" + cvGerman.fileName, decodedBase64German, 'binary', function (err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("German pdf saved!");
+      });
+    }
 
-  //to check if file is loaded correctly
-  // if(req.file){
-  //   cvEnglish =req.file.cvEnglish;
-  //   cvGerman =req.file.cvGerman || "File Not uploaded";
-  // }else{
-  //   // console.log("Error in uploading files.");
-  //   }
+    var desiredPositions = data[1].desiredPositions;
+    var relocationWillingnessFlag = data[3].relocationWillingnes;
+    var country = data[2].country;
+    var city = data[2].city;
+    var visaType = data[2].visaType;
+    var earliestJoiningDate = data[2].earliestJoiningDate;
+    var currentlyEmployedFlag = data[2].currentlyEmployedFlag || false;
+    var drivingPermitFlag = data[2].drivingPermitFlag;
+    var contactNumber = data[2].contactNumber;
+    var noticePeriod = data[2].noticePeriod;
+    var desiredEmployment = data[3].desiredEmployment;
+    var careerLevel = data[4].careerLevel;
+    var industries = data[4].industries;
+    var skills = data[4].skills;
+    var workExperience = data[4].workExperience;
+    var languages = data[4].languages;
+    var onlineProfiles = data[3].onlineProfiles;
+    var cityPreferences = data[3].cityPreferences;
+    var countryPreferences = data[3].countryPreferences;
 
-    // console.log(req.files['cvEnglish'][0].filename);
-    // console.log(req.files['cvGerman'][0].filename);
 
-  var desiredPositions = data[1].desiredPositions;
-  var relocationWillingnessFlag = data[3].relocationWillingnes;
+    // Form Validator
+    // req.checkBody('data','No data input').notEmpty();
+    // req.checkBody('firstName','First Name field is required').notEmpty();
+    // req.checkBody('lastname','Last Name field is required').notEmpty();
+    // req.checkBody('email','Email field is required').notEmpty();
+    // req.checkBody('email','Email is not valid').isEmail();
+    // req.checkBody('password','Password field is required').notEmpty();
+    // req.checkBody('cvEnglish','English CV is required').notEmpty();
+    // req.checkBody('country','Country field is required').notEmpty();
+    // req.checkBody('city','City field is required').notEmpty();
+    // req.checkBody('visaType','Visa field is required').notEmpty();
+    // req.checkBody('earliestJoiningDate','Joining Date is required').notEmpty();
+    // req.checkBody('contactNumber','Contact Number is required').notEmpty();
+    // req.checkBody('cvEnglish','English CV is required').notEmpty();
+    // req.checkBody('password2','Passwords do not match').equals(req.body.password);
 
-  var country = data[2].country;
-  var city = data[2].city;
-  var visaType = data[2].visaType;
-  var earliestJoiningDate = data[2].earliestJoiningDate;
-  var currentlyEmployedFlag = data[2].currentlyEmployedFlag || false;
-  var drivingPermitFlag = data[2].drivingPermitFlag;
-  var contactNumber = data[2].contactNumber;
-  var noticePeriod = data[2].noticePeriod;
-  var desiredEmployment = data[3].desiredEmployment;
+    // Check Errors
+    // var errors = req.validationErrors();
 
-  var careerLevel = data[4].careerLevel;
-  var industries = data[4].industries;
-  var skills = data[4].skills;
-  var workExperience = data[4].workExperience;
-  var languages = data[4].languages;
-  var onlineProfiles = data[3].onlineProfiles;
-
-  var cityPreferences = data[3].cityPreferences;
-  var countryPreferences = data[3].countryPreferences;
-  
-
-  // Form Validator
-  // req.checkBody('data','No data input').notEmpty();
-  // req.checkBody('firstName','First Name field is required').notEmpty();
-  // req.checkBody('lastname','Last Name field is required').notEmpty();
-  // req.checkBody('email','Email field is required').notEmpty();
-  // req.checkBody('email','Email is not valid').isEmail();
-  // req.checkBody('password','Password field is required').notEmpty();
-  // req.checkBody('cvEnglish','English CV is required').notEmpty();
-  // req.checkBody('country','Country field is required').notEmpty();
-  // req.checkBody('city','City field is required').notEmpty();
-  // req.checkBody('visaType','Visa field is required').notEmpty();
-  // req.checkBody('earliestJoiningDate','Joining Date is required').notEmpty();
-  // req.checkBody('contactNumber','Contact Number is required').notEmpty();
-  // req.checkBody('cvEnglish','English CV is required').notEmpty();
-  // req.checkBody('password2','Passwords do not match').equals(req.body.password);
-
-  // Check Errors
-  // var errors = req.validationErrors();
 
     var userData = {};
 
@@ -347,8 +338,8 @@ router.post('/register' ,function(req, res, next) {
     userData.activeJobSeeking = activeJobSeeking;
     userData.termsAndPrivacyFlag = termsAndPrivacyFlag;
     userData.password = password
-    // userData.cvEnglish = req.files['cvEnglish'][0].filename;
-    // userData.cvGerman = req.files['cvGerman'][0].filename || null;
+    userData.cvEnglish = cvEnglish.fileName;
+    userData.cvGerman = cvGerman.fileName;
     userData.desiredPositions = desiredPositions;
     userData.relocationWillingnessFlag = relocationWillingnessFlag;
     userData.desiredEmployment = desiredEmployment;
@@ -368,22 +359,20 @@ router.post('/register' ,function(req, res, next) {
     userData.skills = skills;
     userData.workExperience = workExperience;
     userData.languages = languages;
-
+    
     var newUser = new User(userData);
-
     User.createUser(newUser, function(err, user){
       if(err) throw err;
-      console.log(user);
       userData = user;
       res.send({
         success:true,
         candidate_id:user._id,
       });
     });
-  }catch(err){
+  } catch (err) {
     res.send({
-      success:false,
-      errors:err,
+      success: false,
+      errors: err,
     })
   }
 });
